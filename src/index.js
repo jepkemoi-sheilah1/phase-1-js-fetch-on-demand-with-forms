a1c386b4981345106e7d63a092b8533c7b4fa12e
Jepkemoi Sheilah <jepkemoishyllah@gmail.com>
2025-03-20 07:48:37 +0300

    completed lab using fetch form

--- src/index.js
+++ src/index.js
@@ -1,22 +1,20 @@
-
-document.addEventListener('DOMContentLoaded', () => {
-  const inputForm = document.querySelector('inputForm'); // select the form
-    inputForm.addEventListener('submit', function(event) {
-        event.preventDefault();
-        console.log('form submitted');
-    }); // add event listener to form
-});
-const inputForm = document.querySelector("#searchByID"); 
-fetchMoviesById = (input.value) function fetchMoviesById(moviesId) {
-    fetch(`http://localhost:3000/movies/${moviesId}`)
-    .then(response => response.json())
-    .then(data => {
-        console.log(data);
-    })
-}
-function renderMovies(data) {
-    const title = document.querySelector('#movieDetails h4');
-    const summary = document.querySelector('#movieDetails p');
-    title.innerText = data.title;
-    summary.innerText = data.summary;
-}
+const init = () => {
+    const inputForm = document.querySelector("form");
+  
+    inputForm.addEventListener("submit", (event) => {
+      event.preventDefault();
+      const input = document.querySelector("input#searchByID");
+  
+      fetch(`http://localhost:3000/movies/${input.value}`)
+        .then((response) => response.json())
+        .then((data) => {
+          const title = document.querySelector("section#movieDetails h4");
+          const summary = document.querySelector("section#movieDetails p");
+  
+          title.innerText = data.title;
+          summary.innerText = data.summary;
+        });
+    });
+  };
+  
+  document.addEventListener("DOMContentLoaded", init);
